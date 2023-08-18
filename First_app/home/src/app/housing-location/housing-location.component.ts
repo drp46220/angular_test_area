8abbcb0c09295bf275cc3967c7ba59429d4af7c5
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-08-18 14:10:42 -0500

    expect a passed value in export HLocation

--- First_app/home/src/app/housing-location/housing-location.component.ts
+++ First_app/home/src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -12,6 +13,9 @@ import { CommonModule } from '@angular/common';
   `,
   styleUrls: ['./housing-location.component.css']
 })
-export class HousingLocationComponent {
 
+// needs the "!" since this is expecting a value to be passed
+// "!" is a non-null assertion operator
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
