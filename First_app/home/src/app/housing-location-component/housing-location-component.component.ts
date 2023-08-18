33ffd13b423a1748836436695d4c4c4de09aa1c4
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-08-18 13:09:13 -0500

    new component

---
+++ First_app/home/src/app/housing-location-component/housing-location-component.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location-component',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      housing-location-component works!
+    </p>
+  `,
+  styleUrls: ['./housing-location-component.component.css']
+})
+export class HousingLocationComponentComponent {
+
+}
