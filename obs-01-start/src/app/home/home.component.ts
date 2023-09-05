6b9c88b261ebd9ecc7dcbae89a90989ebce77083
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-05 14:45:28 -0500

    first custom observable

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private sub: Subscription;
 
   constructor() { }
 
+
   ngOnInit() {
+    // build user defined observablesç
+    this.sub = interval(1000).subscribe(count => {
+      console.log(count + "seconds");
+    });
   }
 
+  // prevetns memory leaks
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
