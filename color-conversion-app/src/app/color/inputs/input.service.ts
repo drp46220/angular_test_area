31cbcef42f3d4bb089bfe407614179e45f2d58cc
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-07 10:41:33 -0500

    added a color panel and convert all colors. Done

--- color-conversion-app/src/app/color/inputs/input.service.ts
+++ color-conversion-app/src/app/color/inputs/input.service.ts
@@ -2,73 +2,143 @@ import { Injectable } from '@angular/core';
 import { CMYK, HEX, RGB } from '../color-model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InputService {
-
   string_to_rgb(s: string) {
-    return new RGB(Number(s.split(","[0])), Number(s.split(",")[1]), Number(s.split(",")[2]))
+    return new RGB(
+      Number(s.split(',')[0]),
+      Number(s.split(',')[1]),
+      Number(s.split(',')[2])
+    );
   }
 
   string_to_cmyk(s: string) {
-    return new CMYK(Number(s.split(","[0])), Number(s.split(",")[1]), Number(s.split(",")[2]), Number(s.split(",")[3]));
+    return new CMYK(
+      Number(s.split(',')[0]),
+      Number(s.split(',')[1]),
+      Number(s.split(',')[2]),
+      Number(s.split(',')[3])
+    );
   }
 
   string_to_hex(s: string) {
-    return new HEX(s)
+    return new HEX(s);
   }
 
   rgb_to_cmyk(color: string) {
     const RGBcolor = this.string_to_rgb(color); // convert rgb string to rgb color type
 
-    const r: number = (RGBcolor.red / 255)
-    const g: number = (RGBcolor.green / 255)
-    const b: number = (RGBcolor.blue / 255)
+    console.log(
+      `rgbToCMYK--before math r:${RGBcolor.red}, g:${RGBcolor.green}, b:${RGBcolor.blue}, Max:${RGBcolor.rangeMax}, Min:${RGBcolor.rangeMin}`
+    );
 
-    const k: number = +(1 - Math.max(r, g, b)).toFixed(1);
-    const c: number = +((1 - r - k) / (1 - k) || 0).toFixed(1);
-    const m: number = +((1 - g - k) / (1 - k) || 0).toFixed(1);
-    const y: number = +((1 - b - k) / (1 - k) || 0).toFixed(1);
+    // these stay global so i probaly shouldnt use this method in the future
+    const r: number = 1 - RGBcolor.red / RGBcolor.rangeMax;
+    const g: number = 1 - RGBcolor.green / RGBcolor.rangeMax;
+    const b: number = 1 - RGBcolor.blue / RGBcolor.rangeMax;
 
-    return new CMYK(c, m, y, k)
+    var black: number;
+
+    console.log(
+      `computed C:${r.toFixed(2)}, computed M:${g.toFixed(
+        2
+      )}, computed Y:${b.toFixed(2)}`
+    );
+
+    var minCMY = Math.min(r, Math.min(g, b));
+    if (
+      RGBcolor.red === 255 ||
+      RGBcolor.green === 255 ||
+      RGBcolor.blue === 255
+    ) {
+      black = 1;
+    } else {
+      black = +minCMY.toFixed(2);
+    }
+
+    const k = black;
+    const c: number = +((r - minCMY) / (1 - minCMY) || 0).toFixed(2);
+    const m: number = +((g - minCMY) / (1 - minCMY) || 0).toFixed(2);
+    const y: number = +((b - minCMY) / (1 - minCMY) || 0).toFixed(2);
+
+    return new CMYK(c * 100, m * 100, y * 100, k * 100);
   }
 
   rgb_to_hex(color: string) {
     const RGBcolor = this.string_to_rgb(color);
 
+    console.log(
+      `rgbToHex-- r:${RGBcolor.red}, g:${RGBcolor.green}, b:${RGBcolor.blue}, Max:${RGBcolor.rangeMax}, Min:${RGBcolor.rangeMin}`
+    );
+
     const r: number = RGBcolor.red;
     const g: number = RGBcolor.green;
     const b: number = RGBcolor.blue;
 
-    function componentToHex(c: number) { // each component to hex
+    function componentToHex(c: number) {
+      // each component to hex
       var hex = c.toString(16);
-      return hex.length == 1 ? "0" + hex : hex;
+      return hex.length == 1 ? '0' + hex : hex;
     }
 
-    function rgbToHex(r: number, g: number, b: number) { // combine components
-      return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    function rgbToHex(r: number, g: number, b: number) {
+      // combine components
+      const h: string =
+        '#' +
+        componentToHex(r).toString().toUpperCase() +
+        componentToHex(g).toString().toUpperCase() +
+        componentToHex(b).toString().toUpperCase();
+      return h;
     }
+
+    return rgbToHex(r, g, b);
   }
 
-  cmyk_to_rgb() { }
+  cmyk_to_rgb(color: string) {
+    const CMYKcolor = this.string_to_cmyk(color); // convert cmyk string to cmyk color type
 
-  cmyk_to_hex() { }
+    console.log(
+      `cmykToRGB--before math r:${CMYKcolor.cyan}, g:${CMYKcolor.magenta}, b:${CMYKcolor.yellow}, k:${CMYKcolor.black}, Max:${CMYKcolor.rangeMax}, Min:${CMYKcolor.rangeMin}`
+    );
+
+    const r1: number =
+      255 *
+      (1 - CMYKcolor.cyan / CMYKcolor.rangeMax) *
+      (1 - CMYKcolor.black / CMYKcolor.rangeMax);
+    const g1: number =
+      255 *
+      (1 - CMYKcolor.magenta / CMYKcolor.rangeMax) *
+      (1 - CMYKcolor.black / CMYKcolor.rangeMax);
+    const b1: number =
+      255 *
+      (1 - CMYKcolor.yellow / CMYKcolor.rangeMax) *
+      (1 - CMYKcolor.black / CMYKcolor.rangeMax);
+
+    console.log(
+      `computed r:${Math.ceil(r1)}, computed g:${Math.ceil(
+        g1
+      )}, computed r:${Math.ceil(b1)}`
+    );
+
+    return new RGB(Math.round(r1), Math.round(g1), Math.round(b1));
+  }
+
+  cmyk_to_hex(color: string) {
+    return this.rgb_to_hex(this.cmyk_to_rgb(color).toSting());
+  }
 
   hex_to_rgb(hex: string) {
-
-    function hexToRgb(hex: string) {
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-      } : null;
-    }
+    return new RGB(
+      +('0x' + hex[1].toUpperCase() + hex[2].toUpperCase()) | 0,
+      +('0x' + hex[3].toUpperCase() + hex[4].toUpperCase()) | 0,
+      +('0x' + hex[5].toUpperCase() + hex[6].toUpperCase()) | 0
+    );
   }
 
   hex_to_cmyk(color: string) {
-    this.hex_to_rgb(this.rgb_to_cmyk(color).toString())
+    return this.rgb_to_cmyk(this.hex_to_rgb(color).toSting());
   }
 
-  constructor() { }
+  constructor() {}
 }
--- color-conversion-app/src/app/color/inputs/inputs.component.ts
+++ color-conversion-app/src/app/color/inputs/inputs.component.ts
@@ -6,42 +6,180 @@ import { InputService } from './input.service';
 @Component({
   selector: 'app-inputs',
   templateUrl: './inputs.component.html',
-  styleUrls: ['./inputs.component.css']
+  styleUrls: ['./inputs.component.css'],
 })
 export class InputsComponent {
+  @ViewChild('rgbForm') rgbForm!: NgForm;
+  @ViewChild('cmykForm') cmykForm!: NgForm;
+  @ViewChild('hexForm') hexForm!: NgForm;
 
-  @ViewChild('color_form', { static: false })
-  color_form!: NgForm;
+  constructor(private inputService: InputService) {}
 
-  constructor(private inputService: InputService) { }
-
-  rgb_string!: string;
-  cmyk_string!: string;
-  hex_string!: string;
+  color_string = {
+    rgb_string: '',
+    cmyk_string: '',
+    hex_string: '',
+  };
 
   rgb_color!: RGB;
   cmyk_color!: CMYK;
   hex_color!: HEX;
 
-  convert() {
-    this.rgb_string = (<HTMLInputElement>document.getElementById("rgbCode")).value
-    this.cmyk_string = (<HTMLInputElement>document.getElementById("cmykCode")).value
-    this.hex_string = (<HTMLInputElement>document.getElementById("hexCode")).value
-    tempCMYK: CMYK;
-    tempHEX: HEX;
-    tempRGB: RGB;
+  submitted: boolean = false;
 
-    if ((<HTMLInputElement>document.getElementById("rgbCode")).value != '') {
-      const tempCMYK = this.inputService.rgb_to_cmyk(this.rgb_string);
-      const tempHEX = this.inputService.rgb_to_hex(this.rgb_string);
+  convertHEX() {
+    this.submitted = true;
+    // console.log(
+    //   `outside of hex if statement-- ${
+    //     this.hexForm.value != '' || this.hexForm.value != null
+    //   }`
+    // );
+    // console.log(
+    //   `if this.hexForm.value != '' || this.hexForm.value != null-- ${
+    //     this.hexForm.value != '' || this.hexForm.value != null
+    //   }`
+    // );
+    // hex is filled out
+    if (this.hexForm.value != '' || this.hexForm.value != null) {
+      // console.log(`in hex if statement--${this.hexForm}`);
+      // console.log(
+      //   `hexElementId = ${
+      //     (<HTMLInputElement>document.getElementById('hexCode')).value
+      //   }`
+      // );
+      // to hex
+      this.hex_color = this.inputService.string_to_hex(
+        (<HTMLInputElement>document.getElementById('hexCode')).value
+      );
+      this.color_string.hex_string = (<HTMLInputElement>(
+        document.getElementById('hexCode')
+      )).value.toUpperCase();
+      // console.log(`hex_form.value= ${this.hexForm.value}`);
+      // console.log(`hex_string= ${this.color_string.hex_string}`);
+      // console.log(`hex_color= ${this.hex_color}`);
+
+      // to rgb
+      const tempRGB = this.inputService.hex_to_rgb(
+        this.color_string.hex_string
+      );
+      this.color_string.rgb_string = tempRGB.toSting();
+
+      //to cmyk
+      this.color_string.cmyk_string = this.inputService
+        .hex_to_cmyk(this.color_string.hex_string)
+        .toSting();
+
+      (<HTMLInputElement>document.getElementById('rgbCode')).value =
+        this.color_string.rgb_string;
+      (<HTMLInputElement>document.getElementById('cmykCode')).value =
+        this.color_string.cmyk_string;
     }
+  }
 
+  convertCMYK() {
+    this.submitted = true;
+    // console.log(
+    //   `outside of cmyk if statement-- ${
+    //     this.cmykForm.value != '' || this.cmykForm.value != null
+    //   }`
+    // );
+    // console.log(
+    //   `if this.cmykForm.value != '' || this.cmykForm.value != null-- ${
+    //     this.cmykForm.value != '' || this.cmykForm.value != null
+    //   }`
+    // );
+    // cnyk is filled out
+    if (this.cmykForm.value != '' || this.cmykForm.value != null) {
+      // console.log(`in cmyk if statement--${this.cmykForm}`);
+      // console.log(
+      //   `cmykElementId = ${
+      //     (<HTMLInputElement>document.getElementById('cmykCode')).value
+      //   }`
+      // );
+      // to cmyk
+      this.cmyk_color = this.inputService.string_to_cmyk(
+        (<HTMLInputElement>document.getElementById('cmykCode')).value
+      );
+      this.color_string.cmyk_string = (<HTMLInputElement>(
+        document.getElementById('cmykCode')
+      )).value;
+      // console.log(`cmyk_form.value= ${this.cmykForm.value}`);
+      // console.log(`cmyk_string= ${this.color_string.cmyk_string}`);
+      // console.log(`cmyk_color= ${this.cmyk_color.toSting()}`);
 
+      // to rgb
+      const tempRGB = this.inputService.cmyk_to_rgb(this.cmyk_color.toSting());
+      this.color_string.rgb_string = tempRGB.toSting();
+
+      // to hex
+      const tempHEX = this.inputService.cmyk_to_hex(this.cmyk_color.toSting());
+      this.color_string.hex_string = tempHEX.toString();
+
+      (<HTMLInputElement>document.getElementById('rgbCode')).value =
+        this.color_string.rgb_string;
+      (<HTMLInputElement>document.getElementById('hexCode')).value =
+        this.color_string.hex_string;
+    }
+  }
+
+  convertRGB() {
+    this.submitted = true;
+
+    // console.log(
+    //   `outside of rgb if statement-- ${
+    //     this.rgbForm.value != '' || this.rgbForm.value != null
+    //   }`
+    // );
+    // console.log(
+    //   `if this.rgbForm.value != '' || this.rgbForm.value != null-- ${
+    //     this.rgbForm.value != '' || this.rgbForm.value != null
+    //   }`
+    // );
+    // rgb is filled out
+    if (this.rgbForm.value != '' || this.rgbForm.value != null) {
+      // console.log(`in rgb if statement--${this.rgbForm}`);
+      // to rgb
+      this.rgb_color = this.inputService.string_to_rgb(
+        (<HTMLInputElement>document.getElementById('rgbCode')).value
+      );
+      this.color_string.rgb_string = (<HTMLInputElement>(
+        document.getElementById('rgbCode')
+      )).value;
+      // console.log(`rgb_form.value= ${this.rgbForm.value}`);
+      // console.log(`rgb_string= ${this.color_string.rgb_string}`);
+      // console.log(`rgb_color= ${this.rgb_color.toSting()}`);
+
+      // to cmyk
+      const tempCMYK = this.inputService.rgb_to_cmyk(this.rgb_color.toSting());
+      this.color_string.cmyk_string = tempCMYK.toSting();
+
+      // to hex
+      const tempHEX = this.inputService.rgb_to_hex(this.rgb_color.toSting());
+      this.color_string.hex_string = tempHEX.toString();
+
+      (<HTMLInputElement>document.getElementById('cmykCode')).value =
+        this.color_string.cmyk_string;
+      (<HTMLInputElement>document.getElementById('hexCode')).value =
+        this.color_string.hex_string;
+    }
   }
 
   clear() {
-    (<HTMLInputElement>document.getElementById("rgbCode")).value = '';
-    (<HTMLInputElement>document.getElementById("cmykCode")).value = '';
-    (<HTMLInputElement>document.getElementById("hexCode")).value = '';
+    // console.log(`before clear ${this.rgbForm}`);
+    // console.log(`before clear ${this.cmykForm}`);
+    // console.log(`before clear ${this.hexForm}`);
+
+    this.rgbForm.reset();
+    this.cmykForm.reset();
+    this.hexForm.reset();
+    this.submitted = false;
+
+    this.color_string.rgb_string = '';
+    this.color_string.cmyk_string = '';
+    this.color_string.hex_string = '';
+
+    // console.log(`after clear ${this.rgbForm}`);
+    // console.log(`after clear ${this.cmykForm}`);
+    // console.log(`after clear ${this.hexForm}`);
   }
 }
