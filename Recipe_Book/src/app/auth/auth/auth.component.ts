3dda757acd1aafd211f9fc7bc7a38525590afb9f
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-12 12:10:23 -0500

    only enter if user authenticated

--- Recipe_Book/src/app/auth/auth/auth.component.ts
+++ Recipe_Book/src/app/auth/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from '../auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit(form: NgForm) {
     if (!form.valid) {
@@ -36,6 +37,7 @@ export class AuthComponent {
       (resData) => {
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       },
       (errorMessage) => {
         console.log(errorMessage);
--- Recipe_Book/src/app/header/header.component.ts
+++ Recipe_Book/src/app/header/header.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  constructor(private data_storage: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  isAuth = false;
+
+  constructor(
+    private data_storage: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe((user) => {
+      // these 2 lines do the same thing
+      // this.isAuth = !user ? false : true;
+      this.isAuth = !!user;
+      // sets (isAuth) to true if not false since it starts (=== false)
+    });
+  }
 
   Save() {
     this.data_storage.recipeStore();
