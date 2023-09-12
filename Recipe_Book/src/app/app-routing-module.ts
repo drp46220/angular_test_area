51c54f3cdffc79d670f011454353b3a5e969ef52
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-12 08:50:18 -0500

    new authentication page with header navigation

--- Recipe_Book/src/app/app-routing-module.ts
+++ Recipe_Book/src/app/app-routing-module.ts
@@ -7,6 +7,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResolveService } from './recipes/recipe-resolve.service';
+import { AuthComponent } from './auth/auth/auth.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' }, // starting path on first load
@@ -30,6 +31,7 @@ const appRoutes: Routes = [
     ],
   },
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
--- Recipe_Book/src/app/app.module.ts
+++ Recipe_Book/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { AppRoutingModule } from './app-routing-module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { RecipeService } from './recipes/recipe.service';
     DropdownDirective,
     RecipeEditComponent,
     RecipeStartComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
