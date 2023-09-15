20caaf31d8775102a61a9ea967e924018af93520
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-15 15:24:00 -0500

    read and output a signal

--- 01 Starting Setup/src/app/signals/signals.component.ts
+++ 01 Starting Setup/src/app/signals/signals.component.ts
@@ -11,14 +11,19 @@ export class SignalsComponent {
   actions: string[] = [];
   // counter = 0; // not a signal
   counter = signal(0); //set variable equal to a signal and pass its desired value
+  // counter is a signal now and has to be called as a function << {{ counter() }} >> in templates. its value will be returned
 
   increment() {
-    this.counter++;
+    // this.counter++; // standard increment without a signal
+    this.counter.update((oldCounter) => oldCounter + 1); // updates counter value oldCounter = counter -> oldCounter + 1 --> counter updated to 1 more
+    // don't forget to use this updated signal in the template
+
     this.actions.push("INCREMENT");
   }
 
   decrement() {
-    this.counter--;
+    // this.counter--; // standard decrement without a counter
+    this.counter.update((oldCounter) => oldCounter - 1);
     this.actions.push("DECREMENT");
   }
 }
