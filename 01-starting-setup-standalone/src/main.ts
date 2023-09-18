a979efda41b69d24a239d50481039e6fe0b25c35
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-17 20:27:53 -0500

    add to providers array in main.ts

--- 01-starting-setup-standalone/src/main.ts
+++ 01-starting-setup-standalone/src/main.ts
@@ -3,8 +3,9 @@ import { provideStore } from '@ngrx/store';
 
 import { AppComponent } from './app/app.component';
 import { counterReducer } from './app/counter-store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideStore({ counter: counterReducer })],
+  providers: [provideStore({ counter: counterReducer }), provideEffects()],
   // an empty store is created to hold data
 });
