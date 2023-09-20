ae44c372ce05b5bb04332560bc72daec97472fe9
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-20 14:54:08 -0500

    testing

--- Recipe_Book/src/app/app.component.spec.ts
+++ Recipe_Book/src/app/app.component.spec.ts
@@ -1,29 +1,35 @@
 // import { TestBed } from '@angular/core/testing';
-// import { AuthComponent } from './auth/auth/auth.component';
+// // import { AuthComponent } from './auth/auth/auth.component';
 // import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-// import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-// import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+// // import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+// // import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 // import { RecipeService } from './recipes/recipe.service';
+// import { AuthComponent } from './auth/auth/auth.component';
+// import { AppRoutingModule } from './app-routing-module';
 
 // describe('component: ', () => {
 //   beforeEach(() => {
 //     TestBed.configureTestingModule({
-//       declarations: [
-//         RecipeStartComponent,
-//         RecipeListComponent,
-//         RecipeItemComponent,
-//         RecipeService,
-//       ],
+//       declarations: [RecipeStartComponent, RecipeService],
 //     });
 //   });
 //   it('recipe start created', () => {
 //     let fixture = TestBed.createComponent(RecipeStartComponent);
 //     let app = fixture.debugElement.componentInstance;
+//     fixture.detectChanges();
 //     expect(app).toBeTruthy();
 //   });
-//   it('recipe created', () => {
-//     let fixture = TestBed.createComponent(RecipeService);
+
+//   it('route created', () => {
+//     let fixture = TestBed.createComponent(AppRoutingModule);
 //     let app = fixture.debugElement.componentInstance;
+//     fixture.detectChanges();
+//     expect(app).toBeTruthy();
+//   });
+//   it('auth created', () => {
+//     let fixture = TestBed.createComponent(AuthComponent);
+//     let app = fixture.debugElement.componentInstance;
+//     fixture.detectChanges();
 //     expect(app).toBeTruthy();
 //   });
 // });
