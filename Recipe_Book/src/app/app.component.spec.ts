8bcc212734ed409d6de32e1ec241520af7cebbc2
drp46220 <82248037+drp46220@users.noreply.github.com>
2023-09-20 14:20:09 -0500

    testing out the .spec.ts functionality

---
+++ Recipe_Book/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+// import { TestBed } from '@angular/core/testing';
+// import { AuthComponent } from './auth/auth/auth.component';
+// import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+// import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+// import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+// import { RecipeService } from './recipes/recipe.service';
+
+// describe('component: ', () => {
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [
+//         RecipeStartComponent,
+//         RecipeListComponent,
+//         RecipeItemComponent,
+//         RecipeService,
+//       ],
+//     });
+//   });
+//   it('recipe start created', () => {
+//     let fixture = TestBed.createComponent(RecipeStartComponent);
+//     let app = fixture.debugElement.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+//   it('recipe created', () => {
+//     let fixture = TestBed.createComponent(RecipeService);
+//     let app = fixture.debugElement.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+// });
