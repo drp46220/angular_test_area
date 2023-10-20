caa0f15a32d1defb1f14ab3f0da75b04adc3ad96
Dominic Prince <dominicprc3@gmail.com>
2023-10-20 12:54:53 -0500

    home and away rosters added

--- Scoreboards/Volleyball/src/app/away-roster/away-roster.component.ts
+++ Scoreboards/Volleyball/src/app/away-roster/away-roster.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { RosterService } from '../roster.service';
+import { Team } from '../team.model';
 
 @Component({
   selector: 'app-away-roster',
   templateUrl: './away-roster.component.html',
-  styleUrls: ['./away-roster.component.css']
+  styleUrls: ['./away-roster.component.css'],
 })
 export class AwayRosterComponent {
+  constructor(public rosterService: RosterService) {}
 
+  teamData: Team[];
+  awayTeam: Team;
+
+  ngOnInit() {
+    // read team data from the service
+    this.rosterService.getTeamData().subscribe((data) => {
+      // console.log('data: ', data);
+      this.teamData = data;
+      this.awayTeam = this.teamData[1]; // Assuming data for Home Team is the first object
+      // console.log(this.awayTeam);
+    });
+  }
+
+  onPlayerClick(player: any) {
+    // Implement the desired behavior when a player is clicked
+    console.log(`Player ${player.name} was clicked.`);
+    // Add your logic here, e.g., to display more information about the player.
+  }
 }
--- Scoreboards/Volleyball/src/app/home-roster/home-roster.component.ts
+++ Scoreboards/Volleyball/src/app/home-roster/home-roster.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { RosterService } from '../roster.service';
+import { Team } from '../team.model';
 
 @Component({
   selector: 'app-home-roster',
   templateUrl: './home-roster.component.html',
-  styleUrls: ['./home-roster.component.css']
+  styleUrls: ['./home-roster.component.css'],
 })
 export class HomeRosterComponent {
+  constructor(public rosterService: RosterService) {}
 
+  teamData: Team[];
+  homeTeam: Team;
+
+  ngOnInit() {
+    // read team data from the service
+    this.rosterService.getTeamData().subscribe((data) => {
+      // console.log('data: ', data);
+      this.teamData = data;
+      this.homeTeam = this.teamData[0]; // Assuming data for Home Team is the first object
+      // console.log(this.homeTeam);
+    });
+  }
+
+  onPlayerClick(player: any) {
+    // Implement the desired behavior when a player is clicked
+    console.log(`Player ${player.name} was clicked.`);
+    // Add your logic here, e.g., to display more information about the player.
+  }
 }
