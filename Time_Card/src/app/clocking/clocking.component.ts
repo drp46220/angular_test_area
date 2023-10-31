bf972b815f7b7d2b5982ad8ae916341b6468e0e5
Dominic Prince <dominicprc3@gmail.com>
2023-10-31 15:32:55 -0500

    add clocking comp to landing page

--- Time_Card/src/app/clocking/clocking.component.ts
+++ Time_Card/src/app/clocking/clocking.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-clocking',
   templateUrl: './clocking.component.html',
-  styleUrls: ['./clocking.component.css']
+  styleUrls: ['./clocking.component.css'],
 })
 export class ClockingComponent {
+  currentDateTime: string;
 
+  ngOnInit(): void {
+    this.updateDateTime();
+    setInterval(() => this.updateDateTime(), 1000); // Update every second
+  }
+
+  updateDateTime() {
+    const now = new Date();
+    this.currentDateTime = now.toISOString(); // You can format this as per your requirement
+  }
 }
